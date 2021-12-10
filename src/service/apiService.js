07865c508fe765cd632979c0be0c0a55b077b230
zahra safarian <zahra.safarian@hotmail.com>
2021-12-10 19:44:34 +0330

    PUSH

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,7 +1,9 @@
 export const getListService = async (page, sort) => {
     console.log("==> " + page)
+    const sortText = sort ? `sort=${sort}&` : "";
+    console.log("@@ " + sortText);
     return await fetch(
-        `https://api.snapp.express/mobile/v2/product-variation/index?filters={}&fetch_filters=1&fetch_categories=1&menu_category_id=390176&vendorCode=po9qzk&page=${page}&client=PWA&optionalClient=PWA&deviceType=PWA&appVersion=5.6.6&optionalVersion=5.6.6&UDID=ca69b564-a46e-4b71-a59b-6e08975ac8d3`
+        `https://api.snapp.express/mobile/v2/product-variation/index?filters={}&${sortText}fetch_filters=1&fetch_categories=1&menu_category_id=390176&vendorCode=po9qzk&page=${page}&client=PWA&optionalClient=PWA&deviceType=PWA&appVersion=5.6.6&optionalVersion=5.6.6&UDID=ca69b564-a46e-4b71-a59b-6e08975ac8d3`
     )
         .then((res) => res.json())
         .then((json) => {
